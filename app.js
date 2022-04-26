13b9361beeae7d011b26cb20b6481c3f4c07cef7
Paula <pauladzuck@hotmail.com>
2022-04-26 14:18:01 -0500

    Adding updates to app.js file.

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+//Delete book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -82,3 +89,17 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
   e.preventDefault();
 });
+
+//Event listener for delete
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+
+  ui.showAlert('Book removed.', 'success');
+
+
+  e.preventDefault();
+})
